ffaf94faf3b3124ba155222645131b40d75468e9
parksaehyun <psh666777@naver.com>
2024-08-06 11:16:00 +0900

    파일 조회, 파일 업로드 수업소스

--- src/main/java/org/choongang/file/services/FileDownloadService.java
+++ src/main/java/org/choongang/file/services/FileDownloadService.java
@@ -1,5 +1,52 @@
 package org.choongang.file.services;
 
-public class FileDownloadService {
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.exceptions.FileNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
+public class FileDownloadService {
+    private final FileInfoService infoService;
+    private final HttpServletResponse response; // 응답헤더를 통해 파일을 다운받음
+
+    // 파일을 불러오고 응답헤더를 통해 파일을 다운받음
+    public void download(Long seq) {
+        FileInfo data = infoService.get(seq); // 파일 조회
+
+        String filePath = data.getFilePath(); // 2차가공한 데이터 가져오기
+        String fileName = new String(data.getFileName().getBytes(), StandardCharsets.ISO_8859_1); // 파일이름 가져오기, 한글깨짐 방지하기 위해 2byte로 변환
+
+        File file = new File(filePath); // 파일이 없으면 예외 던지기
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        String contentType = data.getContentType();
+        contentType = StringUtils.hasText(contentType) ? contentType : "application/octet-stream"; // 콘텐트 타입이 없는 경우 고정해줄 값
+
+        // 파일 가져오기
+        try (FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis)) {
+
+            response.setHeader("Content-Disposition", "attachment; filename=" + fileName); // 응답헤더에 파일 넣어주기
+            response.setContentType(contentType);
+            response.setIntHeader("Expires", 0); // 만료시간 x
+            response.setHeader("Cache-Control", "must-revalidate"); // 캐시 비우기
+            response.setContentLengthLong(file.length()); // 현재 파일 용랑
+
+            OutputStream out = response.getOutputStream();
+            out.write(bis.readAllBytes());
+            // 헤더에 정의하고 // 바디에서 출력
+
+            } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/choongang/file/services/FileInfoService.java
+++ src/main/java/org/choongang/file/services/FileInfoService.java
@@ -1,17 +1,23 @@
 package org.choongang.file.services;
 
+import com.querydsl.core.BooleanBuilder;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.constants.FileStatus;
 import org.choongang.file.entities.FileInfo;
+import org.choongang.file.entities.QFileInfo;
 import org.choongang.file.exceptions.FileNotFoundException;
 import org.choongang.file.repositories.FileInfoRepository;
 import org.choongang.global.configs.FileProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Objects;
+
+import static org.springframework.data.domain.Sort.Order.asc;
 
 @Service
 @RequiredArgsConstructor
@@ -37,7 +43,7 @@ public class FileInfoService {
          * 2. 파일을 접근할 수 있는 PATH - 파일 삭제, 다운로드 등등
          */
 
-        addFileInfo(item);
+        addFileInfo(item); // 추가적인 데이터 넣어주기, db랑 상관없이 데이터 넣어주는거 @Trandient fileUrl, filePath // ex) 썸네일
 
         return item;
     }
@@ -51,8 +57,37 @@ public class FileInfoService {
      * @return
      */
     public List<FileInfo> getList(String gid, String location, FileStatus status) {
+        // gid : 하나의 ??? // location : 하나의 상품에 여러이미지, 리스트이미지, 상세이미지...(필수는 아님) // status : 게시글 올리고 이미지가 바로 보일려면 파일이 서버에 올라가 있어야 함 done, undone, all(작업완료 미완료 모두 보기)
 
-        return null;
+        QFileInfo fileInfo = QFileInfo.fileInfo;
+        BooleanBuilder andBuilder = new BooleanBuilder();
+        andBuilder.and(fileInfo.gid.eq(gid)); // eq : =
+
+        if (StringUtils.hasText(location)) { // 이미지를 리스트 올 상세페이지 등등...
+            andBuilder.and(fileInfo.location.eq(location));
+        }
+
+        if (status != FileStatus.ALL) {
+            andBuilder.and(fileInfo.done.eq(status == FileStatus.DONE));
+        }
+
+        List<FileInfo> items = (List<FileInfo>) infoRepository.findAll(andBuilder, Sort.by(asc("createdAt")));
+
+        // 2차 추가 데이터 처리
+        items.forEach(this::addFileInfo);
+
+        return items;
+    }
+
+    public List<FileInfo> getList(String gid, String location) {
+        // gid를 가장 많이 사용
+        // 조회 : done
+        // 수정, 등록 : undone
+        return getList(gid, location, FileStatus.DONE); // 가장 많이 조회하는게 done이니까 고정
+    }
+
+    public List<FileInfo> getList(String gid) {
+        return getList(gid, null, FileStatus.DONE);
     }
 
     /**
